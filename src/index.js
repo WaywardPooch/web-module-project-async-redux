4f5e5dea2b6be18e944d9b190be8f616f8ac67bc
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-10-06 11:40:32 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Libraries
+import React from "react";
+import ReactDOM from "react-dom";
+// Styles
+import "./index.css";
+// Components
+import App from "./App";
+
+// Render the app
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
